81df5b6573d2a100585eebc8612cfe672685e35d
Naveen <tharunkumar4976@gmail.com>
2026-02-17 10:04:31 +0530

    first commit

---
+++ src/PallindromeChecker.java
@@ -0,0 +1,9 @@
+public class PallindromeChecker{
+    public static void main(String[] args){
+        System.out.println("Welcome to Pallindrome checkerapp");
+        System.out.println("Version 1.0");
+
+    }
+
+
+}
