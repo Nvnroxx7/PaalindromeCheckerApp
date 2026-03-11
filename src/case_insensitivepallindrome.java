1cac3b584146656637b1de38e5ea3547a8217d86
Lucius-cloud <ng6831@srmist.edu.in>
2026-03-11 10:17:07 +0530

    UC010: case insensitive space controlled pallindrome

---
+++ src/case_insensitivepallindrome.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class case_insensitivepallindrome {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+
+        int start = 0;
+        int end = normalized.length() - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome)
+            System.out.println("The string is a Palindrome.");
+        else
+            System.out.println("The string is NOT a Palindrome.");
+
+        sc.close();
+    }
+}
